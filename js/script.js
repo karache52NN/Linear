9f0972b92c182b10eef8cd94edf0ceea904162fe
karache52NN <karache@yandex.ru>
2017-05-06 11:17:02 +0300

    Already

--- js/script.js
+++ js/script.js
@@ -14,23 +14,26 @@ function init() {
             secondDigit.classList.add("hidden");
         }
         var numberOne=document.getElementById("numberOne");
-        numberOne.style.width=(event.target.value*38)+"px";
+        numberOne.style.width=(event.target.value*39)+"px";
 
 
     }
     function onchangeHandler2(event) {
         var seconddigit = document.getElementById("extentionSecondNumber");
-        var res=document.getElementById("extentionResult");
+        var resfield=document.getElementById("resOfExtention");
+        var numberTwo=document.getElementById("numberTwo");
+        numberTwo.style.width=(event.target.value*40)+"px";
 
 
         if(event.target.value==4){
             seconddigit.style.backgroundColor = "yellow";
-            res.innerHTML="11";
+            resfield.classList.remove("hidden");
+
 
         }
         else{
             seconddigit.style.backgroundColor = "red";
-            res.innerHTML="?";
+            resfield.classList.add("hidden");
 
         }
         var numberTwo=document.getElementById("numberTwo");
@@ -38,10 +41,28 @@ function init() {
 
 
     }
+    function resChecker (event) {
+        var Rezult= document.getElementById("resOfExtention");
+        var Summand1= document.getElementById("extentionFirstNumber");
+        var Summand2= document.getElementById("extentionSecondNumber");
+        if (event.target.value==11){
+            Rezult.style.backgroundColor="green";
+            Summand1.style.backgroundColor="green";
+            Summand2.style.backgroundColor="green";
+        }
+        else{
+            Rezult.style.backgroundColor="red";
+            Summand1.style.backgroundColor="yellow";
+            Summand2.style.backgroundColor="yellow";
+        }
+    }
 
     var form = document.getElementById("summandOne");
     form.onchange = onchangeHandler1;
     var form1 = document.getElementById("summandTwo");
     form1.onchange = onchangeHandler2;
+    var resultField=document.getElementById("resOfExtention");
+    resultField.onchange= resChecker;
+
 
 }
